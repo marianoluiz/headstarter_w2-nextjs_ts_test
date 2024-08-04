09cf5565e9c59c3fd0e31c09e0e40f397bad72a4
Mariano Luiz King <kmarianu@gmail.com>
2024-08-04 17:00:39 +0800

    added comment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    /* next js with typescript */
     <html lang="en">
       <body className={inter.className}>
         <Header />
